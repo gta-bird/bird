4cb327a1f5e81a26527a2ed10eb5d51227b14118
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2024-07-08 22:48:10 +0300

    init

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use std::thread;
+use winapi::shared::minwindef::{self, HINSTANCE, DWORD, LPVOID, BOOL};
+
+struct ModuleHandle(HINSTANCE);
+unsafe impl Send for ModuleHandle {}
+
+fn entry(module_handle: ModuleHandle) {
+    let _ = module_handle.0;
+}
+
+#[dllmain_rs::entry]
+fn dllmain() {
+    let module_handle = ModuleHandle(dll_module);
+
+    thread::spawn(move || {
+        entry(module_handle);
+    });
+}
